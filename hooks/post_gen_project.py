62a75be7700ab9ee52577f53ffb519b585b782a4
Jacopo Abramo <jacopo.abramo@gmail.com>
2024-11-15 18:41:41 +0100

    Delete non-necessary files after generation

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,12 +1,56 @@
 #!/usr/bin/env python
 import pathlib
+import os
 
 
 if __name__ == '__main__':
 
-    if '{{ cookiecutter.create_author_file }}' != 'y':
-        pathlib.Path('AUTHORS.rst').unlink()
-        pathlib.Path('docs', 'authors.rst').unlink()
-        
     if 'Not open source' == '{{ cookiecutter.open_source_license }}':
         pathlib.Path('LICENSE').unlink()
+
+    if '{{ cookiecutter._plugin_model_type }}' == 'Motor':
+        to_delete = '_model_detector.py'
+        to_keep = '_model_motor.py'
+    else: # 'Detector'
+        to_delete = '_model_motor.py'
+        to_keep = '_model_detector.py'
+    # TODO: add support for other model types
+    path_to_keep = os.path.join(
+            "src",
+            "{{ cookiecutter.plugin_slug }}",
+            "engine",
+            "{{ cookiecutter._plugin_engine }}",
+            to_keep
+        )
+    path_to_delete = os.path.join(
+            "src",
+            "{{ cookiecutter.plugin_slug }}",
+            "engine",
+            "{{ cookiecutter._plugin_engine }}",
+            to_delete
+        )
+    os.remove(
+        os.path.join(
+            "src",
+            "{{ cookiecutter.plugin_slug }}",
+            "engine",
+            "{{ cookiecutter._plugin_engine }}",
+            to_delete
+        )
+    )
+    os.rename(
+        os.path.join(
+            "src",
+            "{{ cookiecutter.plugin_slug }}",
+            "engine",
+            "{{ cookiecutter._plugin_engine }}",
+            to_keep
+        ),
+        os.path.join(
+            "src",
+            "{{ cookiecutter.plugin_slug }}",
+            "engine",
+            "{{ cookiecutter._plugin_engine }}",
+            "model.py"
+        )
+    )
