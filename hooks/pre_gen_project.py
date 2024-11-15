be564d7ffbacca8519ce44e70af0acf56c59b1c9
Jacopo Abramo <jacopo.abramo@gmail.com>
2024-11-15 18:40:28 +0100

    Do small rename

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -1,12 +1,12 @@
+#!/usr/bin/env python
 import re
 import sys
 
-
 MODULE_REGEX = r'^[_a-zA-Z][_a-zA-Z0-9]+$'
 
-module_name = '{{ cookiecutter.plugin_slug}}'
+module_name = '{{cookiecutter.plugin_slug}}'
 
 if not re.match(MODULE_REGEX, module_name):
-    print('ERROR: The project slug (%s) is not a valid Python module name. '
+    print('ERROR: The plugin slug (%s) is not a valid Python module name. '
           'Please do not use a - and use _ instead' % module_name)
     sys.exit(1)
